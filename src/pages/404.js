093bd26d9ad7294f8ff14c09bfa19926014d2036
Rena Feng <68396709+renapfeng@users.noreply.github.com>
2021-06-29 19:01:42 -0400

    Update 404.js

--- src/pages/404.js
+++ src/pages/404.js
@@ -37,7 +37,7 @@ const notFound = () => {
             fill="currentColor"
           />
         </svg>
-        <p>Either you are in a wrong page or you have lost. Lets go back <Link to="/">home</Link> safely</p>
+        <p>The page you are looking for does not exist. Let's go back <a href="">The Global Dialogue Initiative</a> safely. </p>
       </div>
     </Layout>
   )
