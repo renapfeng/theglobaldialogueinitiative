4dd521ea1cdbd1fdb3a68aea5c776f2bec400ff8
Rena Feng <68396709+renapfeng@users.noreply.github.com>
2021-06-30 17:54:29 -0400

    Update index.js

--- src/pages/index.js
+++ src/pages/index.js
@@ -23,7 +23,7 @@ const IndexPage = ({
         <meta name="description" content={site.siteMetadata.description} />
       </Helmet>
       <HeroHeader/>
-      <h2>Blog Posts &darr;</h2>
+      <h2>Articles written by our youth staff! &darr;</h2>
       <div className="grids">
         {Posts}
       </div>
