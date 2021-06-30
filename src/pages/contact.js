c5b494e084ad5d0f392a87765a6ef9327b7a7ae7
Rena Feng <68396709+renapfeng@users.noreply.github.com>
2021-06-30 18:10:46 -0400

    Update contact.js

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -19,28 +19,7 @@ const ContactPage = ({
           <h1 className="post-title">Get in Touch</h1>
           <p>Let me help you kick start your hello &rarr;</p>
         </div>
-        <div> /*
-          <form className="form-container" action="https://sendmail.w3layouts.com/SubmitContactForm" method="post">
-            <div>
-              <label htmlFor="w3lName">Name</label>
-              <input type="text" name="w3lName" id="w3lName"/>
-            </div>
-            <div>
-              <label htmlFor="w3lSender">Email</label>
-              <input type="email" name="w3lSender" id="w3lSender"/>
-            </div>
-            <div>
-              <label htmlFor="w3lSubject">Subject</label>
-              <input type="text" name="w3lSubject" id="w3lSubject"/>
-            </div>
-            <div>
-              <label htmlFor="w3lMessage">Message</label>
-              <textarea name="w3lMessage" id="w3lMessage"></textarea>
-            </div>
-            <div style={{display: "flex", justifyContent: "flex-end"}}>
-              <input type="submit" className="button -primary" style={{marginRight: 0}} />
-            </div>
-          </form> */
+        <div> */
         </div>
       </div>
     </Layout>
