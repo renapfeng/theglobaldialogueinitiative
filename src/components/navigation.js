72371a360af8b28da373d9220bceaae484cde9cb
Rena Feng <68396709+renapfeng@users.noreply.github.com>
2021-06-30 18:41:31 -0400

    Update navigation.js

--- src/components/navigation.js
+++ src/components/navigation.js
@@ -4,8 +4,8 @@ import ThemeChanger from "../components/themeChanger"
 
 export default (props) => (
   <nav className="navigation"> 
-    <Link to="/contact">Contact</Link>
     <Link to="/about">About</Link>
+    <Link to="/contact">Contact</Link>
     <ThemeChanger/>
   </nav>
   
