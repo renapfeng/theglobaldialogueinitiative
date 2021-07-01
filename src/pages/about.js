9a1df7b61b5f93f91abebb35d296337745b592a0
Rena Feng <68396709+renapfeng@users.noreply.github.com>
2021-06-30 21:01:33 -0400

    Update about.js

--- src/pages/about.js
+++ src/pages/about.js
@@ -17,9 +17,7 @@ const AboutPage = ({
       </Helmet>
       <div className="post-thumbnail" style={{backgroundImage: `url('/assets/alexander-andrews-HgUDpaGPTEA-unsplash.jpg')`, marginBottom: 0}}>
         <h1 className="post-title">About</h1>
-        <p>The Global Dialogue Initiative is a nonpartisan platform where teens can contribute to discussions 
-        <br>about political, economic, and social issues from around the world.
-        </p>
+        <p>The Global Dialogue Initiative is a nonpartisan platform where youth around the world contribute to discussions about political, economic, and social issues.</p>
       </div>
       <div>
         <h3>Name &bull; Executive Director</h3>
