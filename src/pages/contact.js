fb3ee146d629c84398c1c46a654c386916e6ce60
Rena Feng <68396709+renapfeng@users.noreply.github.com>
2021-07-04 18:42:36 -0400

    Update contact.js

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -18,36 +18,9 @@ const ContactPage = ({
       <div className="post-thumbnail" style={{backgroundImage: `url('/assets/alexander-andrews-HgUDpaGPTEA-unsplash.jpg')`, marginBottom: 0}}>
         <h1 className="post-title">Contact</h1>
         <p>The Global Dialogue Initiative is a platform for youth around the world to contribute to discussions on political, economic, and social issues.</p>
+        <Link to='/about' className="button -primary">Email nsjkdf</Link>
       </div>
       <div>
-        <br/>
-        <br/>
-        <h3>Name &bull; Executive Director</h3>
-        <p>Let me help you kick start your next project &rarr;</p>
-        <br/>
-        <br/>
-        <h3>Name &bull; Marketing/Outreach Director</h3>
-        <p>Let me help you kick start your next project &rarr;</p>
-        <br/>
-        <br/>
-        <h3>Name &bull; Creative Director</h3>
-        <p>Let me help you kick start your next project &rarr;</p>
-        <br/>
-        <br/>
-        <h3>Name &bull; Technology Director</h3>
-        <p>Let me help you kick start your next project &rarr;</p>
-        <br/>
-        <br/>
-        <h3>Name &bull; ETC Director</h3>
-        <p>Let me help you kick start your next project &rarr;</p>
-        <br/>
-        <br/>
-        <h3>Name &bull; ETC Director</h3>
-        <p>Let me help you kick start your next project &rarr;</p>
-        <br/>
-        <br/>
-        <h3>Name &bull; ETC Director</h3>
-        <p>Let me help you kick start your next project &rarr;</p>
       </div>
     </Layout>
   )
