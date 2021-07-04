4e0c6e6a1fc470d9e0a9333c4c33e6fcef2ff56e
Rena Feng <68396709+renapfeng@users.noreply.github.com>
2021-07-04 18:37:10 -0400

    Update contact.js

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -1,4 +1,4 @@
-  
+ 
 import React from "react"
 import Helmet from "react-helmet"
 import { graphql } from 'gatsby'
@@ -10,19 +10,43 @@ const ContactPage = ({
   },
 }) => {
   return (
-    </Layout>
+    <Layout>
       <Helmet>
         <title>Contact — {site.siteMetadata.title}</title>
         <meta name="description" content={"Contact page of " + site.siteMetadata.description} />
       </Helmet>
       <div className="post-thumbnail" style={{backgroundImage: `url('/assets/alexander-andrews-HgUDpaGPTEA-unsplash.jpg')`, marginBottom: 0}}>
         <h1 className="post-title">Contact</h1>
-        <p>Wireissues.</p>
+        <p>The Global Dialogue Initiative is a platform for youth around the world to contribute to discussions on political, economic, and social issues.</p>
       </div>
       <div>
         <br/>
         <br/>
         <h3>Name &bull; Executive Director</h3>
+        <p>Let me help you kick start your next project &rarr;</p>
+        <br/>
+        <br/>
+        <h3>Name &bull; Marketing/Outreach Director</h3>
+        <p>Let me help you kick start your next project &rarr;</p>
+        <br/>
+        <br/>
+        <h3>Name &bull; Creative Director</h3>
+        <p>Let me help you kick start your next project &rarr;</p>
+        <br/>
+        <br/>
+        <h3>Name &bull; Technology Director</h3>
+        <p>Let me help you kick start your next project &rarr;</p>
+        <br/>
+        <br/>
+        <h3>Name &bull; ETC Director</h3>
+        <p>Let me help you kick start your next project &rarr;</p>
+        <br/>
+        <br/>
+        <h3>Name &bull; ETC Director</h3>
+        <p>Let me help you kick start your next project &rarr;</p>
+        <br/>
+        <br/>
+        <h3>Name &bull; ETC Director</h3>
         <p>Let me help you kick start your next project &rarr;</p>
       </div>
     </Layout>
