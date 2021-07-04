5d78d795decb80bc5bfa1b24193fd07f3a350a66
Rena Feng <68396709+renapfeng@users.noreply.github.com>
2021-07-04 16:53:20 -0400

    Update contact.js

--- src/pages/contact.js
+++ src/pages/contact.js
@@ -18,7 +18,7 @@ const ContactPage = ({
       <div className="two-grids -contact">
         <div className="post-thumbnail" style={{backgroundImage: `url('/assets/alexander-andrews-HgUDpaGPTEA-unsplash.jpg')`, marginBottom: 0}}>
           <h1 className="post-title">Get in Touch</h1>
-          <p>Let me help you kick start your next project &rarr;</p>
+          <p>Write to us &rarr;</p>
         </div>
         <div>
           <form className="form-container" action="https://sendmail.w3layouts.com/SubmitContactForm" method="post">
