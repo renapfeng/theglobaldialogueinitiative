3e6144ecf9d4c1e611df6558ccaa2b708334bd7d
vivian215 <vivzmm35@gmail.com>
2021-08-09 11:24:42 -0700

    testing

--- src/components/post-link.js
+++ src/components/post-link.js
@@ -14,7 +14,7 @@ const PostLink = ({ post }) => (
           {post.frontmatter.date}
         </Link>
       </h2>
-      <div className="post-meta">{post.frontmatter.author}</div>
+      <div className="post-meta">testing</div>
       <div className="post-author">{post.frontmatter.author}</div>
     </header>
   </article>
